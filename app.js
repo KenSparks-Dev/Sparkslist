a154397db8362e2f1543ead4080a00a7311cf4aa
Ken Sparks <kensparks.dev@gmail.com>
2020-04-18 02:29:53 -0400

    new variables

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-fucntion listItem(){
-  let newItem;
-  newItem.prompt();
-  return prompt()  
+/*  
+What do we want to do?
+
+Essentially what we want in lay mans terms is for a user to enter a line of text into an input and press a button. When that button is pressed the text they've entered is saved a becomes apart of the DOM. We also want to record when a the new list item has been created. 
+
+
+*/ 
+const id = Math.floor(Math.random() * 9999 );
+const title = document.getElementsByClassName("list-item-title");
+const date = new Date();
+
+console.log(date)
+
+
+function createTodo(){
+let newItem;
+ newItem.prompt();
+  return prompt()
 }
